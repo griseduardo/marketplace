4414a156b3940575180cefb7e569383cbdf543ad
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-08-30 01:09:19 -0300

    Validação de subcategorias de produto (testes unitários)

--- app/models/product_subcategory.rb
+++ app/models/product_subcategory.rb
@@ -1,3 +1,10 @@
 class ProductSubcategory < ApplicationRecord
   belongs_to :product_category
+
+  validates :name, presence: true
+  validates :name, uniqueness: { case_sensitive: false }, unless: :equal_others?
+
+  def equal_others?
+    name.to_s.capitalize == 'Outros'
+  end
 end
--- spec/models/product_subcategory_spec.rb
+++ spec/models/product_subcategory_spec.rb
@@ -1,4 +1,58 @@
 require 'rails_helper'
 
-RSpec.describe ProductSubcategory, type: :model do
+describe ProductSubcategory, type: :model do
+  context 'validation' do
+    it 'attributes cannot be blank' do
+      product_subcategory = ProductSubcategory.new
+
+      product_subcategory.valid?
+
+      expect(product_subcategory.errors[:name]).to include('não pode ficar em branco')
+      expect(product_subcategory.errors[:product_category]).to include('é obrigatório(a)')
+    end
+
+    it 'name must be uniq if different from Outros' do
+      product_category = ProductCategory.create!(name: 'Esporte e lazer')
+      another_product_category = ProductCategory.create!(name: 'Fisioterapia')
+      ProductSubcategory.create!(name: 'Bola', product_category: product_category)
+      product_subcategory = ProductSubcategory.new(name: 'Bola', product_category: another_product_category)
+
+      product_subcategory.valid?
+
+      expect(product_subcategory.errors[:name]).to include('já está em uso')
+    end
+
+    it 'name accepts Outros repetition' do
+      product_category = ProductCategory.create!(name: 'Esporte e lazer')
+      another_product_category = ProductCategory.create!(name: 'Eletrodomésticos')
+      ProductSubcategory.create!(name: 'Outros', product_category: product_category)
+      product_subcategory = ProductSubcategory.new(name: 'Outros', product_category: another_product_category)
+
+      product_subcategory.valid?
+
+      expect(product_subcategory.errors[:name]).not_to include('já está em uso')
+    end
+
+    it 'name uniqueness is not case sensitive' do
+      product_category = ProductCategory.create!(name: 'Esporte e lazer')
+      another_product_category = ProductCategory.create!(name: 'Fisioterapia')
+      ProductSubcategory.create!(name: 'Bola', product_category: product_category)
+      product_subcategory = ProductSubcategory.new(name: 'bola', product_category: another_product_category)
+
+      product_subcategory.valid?
+
+      expect(product_subcategory.errors[:name]).to include('já está em uso')
+    end
+
+    it 'name accepts Outros repetition (is not case sensitive)' do
+      product_category = ProductCategory.create!(name: 'Esporte e lazer')
+      another_product_category = ProductCategory.create!(name: 'Eletrodomésticos')
+      ProductSubcategory.create!(name: 'outros', product_category: product_category)
+      product_subcategory = ProductSubcategory.new(name: 'outros', product_category: another_product_category)
+
+      product_subcategory.valid?
+
+      expect(product_subcategory.errors[:name]).not_to include('já está em uso')      
+    end
+  end
 end
