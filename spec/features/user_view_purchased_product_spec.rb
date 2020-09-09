9a5d7c418b4dc3fd46e08e86c283b1b923f15451
Eduardo Henrique Gris <gris.eduardo@gmail.com>
2020-09-09 02:34:23 -0300

    Vendedor e comprador são notificados e visualizam lista de produtos em negociação

--- spec/features/user_view_purchased_product_spec.rb
+++ spec/features/user_view_purchased_product_spec.rb
@@ -422,4 +422,142 @@ feature 'User view purchased product' do
     expect(page).not_to have_content('A bola foi usada na copa?')
     expect(page).not_to have_content('Somente na copa do meu bairro')
   end
+
+  scenario 'and seller is notified about sell negotiation in progress' do
+    Company.create!(name: 'Rotes', cnpj: '96.672.638/0001-48', domain: 'rotes.com')
+    user1 = User.create!(email: 'eduardo@rotes.com', password: '120456')
+    user2 = User.create!(email: 'carol@rotes.com', password: '123456')
+    department1 = Department.create!(name: 'RH')
+    department2 = Department.create!(name: 'Vendas')
+    profile1 = Profile.create!(full_name: 'Eduardo Rodrigues', birthday: '10/03/1992', 
+                               work_address: 'Avenida Paulista, 170', position: 'Assistente', sector: 'Treinamento e desenvolvimento', 
+                               department: department1, user: user1)
+    profile2 = Profile.create!(full_name: 'Edgar Gomes', chosen_name: 'Carol Gomes', birthday: '10/08/1990', 
+                               work_address: 'Avenida Paulista, 170', position: 'Auxiliar', sector: 'Administração', 
+                               department: department2, user: user2)
+    product_category2 = ProductCategory.create!(name: 'Esporte e lazer')
+    product_subcategory2 = ProductSubcategory.create!(name: 'Bola', product_category: product_category2)
+    product_condition2 = ProductCondition.create!(name: 'Usado')
+    product2 = Product.new(name: 'Adidas Fevernova - Bola da copa de 2002', product_subcategory: product_subcategory2, 
+                           description: 'Bola de futebol oficial da Copa do Mundo FIFA de 2002 feita pela Adidas.', price: '240', 
+                           product_condition: product_condition2, quantity: '3', profile: profile2)
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova1.jpg')), filename: 'fevernova1.jpg')
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova2.jpg')), filename: 'fevernova2.jpg')
+    product2.save!
+    total_quantity = 2
+    purchased_product = PurchasedProduct.create!(product: product2, profile: profile1, total_quantity: '2', 
+                                                 initial_value: total_quantity * product2.price, start_date: '10/04/2030')
+    purchased_product.andamento!
+
+    login_as(user2, scope: :user)
+    visit root_path
+
+    expect(page).to have_content('Venda em negociação:')
+    expect(page).to have_content('Adidas Fevernova - Bola da copa de 2002')
+  end
+
+  scenario 'and buyer is notified about purchase negotiation in progress' do
+    Company.create!(name: 'Rotes', cnpj: '96.672.638/0001-48', domain: 'rotes.com')
+    user1 = User.create!(email: 'eduardo@rotes.com', password: '120456')
+    user2 = User.create!(email: 'carol@rotes.com', password: '123456')
+    department1 = Department.create!(name: 'RH')
+    department2 = Department.create!(name: 'Vendas')
+    profile1 = Profile.create!(full_name: 'Eduardo Rodrigues', birthday: '10/03/1992', 
+                               work_address: 'Avenida Paulista, 170', position: 'Assistente', sector: 'Treinamento e desenvolvimento', 
+                               department: department1, user: user1)
+    profile2 = Profile.create!(full_name: 'Edgar Gomes', chosen_name: 'Carol Gomes', birthday: '10/08/1990', 
+                               work_address: 'Avenida Paulista, 170', position: 'Auxiliar', sector: 'Administração', 
+                               department: department2, user: user2)
+    product_category2 = ProductCategory.create!(name: 'Esporte e lazer')
+    product_subcategory2 = ProductSubcategory.create!(name: 'Bola', product_category: product_category2)
+    product_condition2 = ProductCondition.create!(name: 'Usado')
+    product2 = Product.new(name: 'Adidas Fevernova - Bola da copa de 2002', product_subcategory: product_subcategory2, 
+                           description: 'Bola de futebol oficial da Copa do Mundo FIFA de 2002 feita pela Adidas.', price: '240', 
+                           product_condition: product_condition2, quantity: '3', profile: profile2)
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova1.jpg')), filename: 'fevernova1.jpg')
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova2.jpg')), filename: 'fevernova2.jpg')
+    product2.save!
+    total_quantity = 2
+    purchased_product = PurchasedProduct.create!(product: product2, profile: profile1, total_quantity: '2', 
+                                                 initial_value: total_quantity * product2.price, start_date: '10/04/2030')
+    purchased_product.andamento!
+
+    login_as(user1, scope: :user)
+    visit root_path
+
+    expect(page).to have_content('Compra em negociação:')
+    expect(page).to have_content('Adidas Fevernova - Bola da copa de 2002')
+  end
+
+  scenario 'and seller visualize list of negotiation sells' do
+    Company.create!(name: 'Rotes', cnpj: '96.672.638/0001-48', domain: 'rotes.com')
+    user1 = User.create!(email: 'eduardo@rotes.com', password: '120456')
+    user2 = User.create!(email: 'carol@rotes.com', password: '123456')
+    department1 = Department.create!(name: 'RH')
+    department2 = Department.create!(name: 'Vendas')
+    profile1 = Profile.create!(full_name: 'Eduardo Rodrigues', birthday: '10/03/1992', 
+                               work_address: 'Avenida Paulista, 170', position: 'Assistente', sector: 'Treinamento e desenvolvimento', 
+                               department: department1, user: user1)
+    profile2 = Profile.create!(full_name: 'Edgar Gomes', chosen_name: 'Carol Gomes', birthday: '10/08/1990', 
+                               work_address: 'Avenida Paulista, 170', position: 'Auxiliar', sector: 'Administração', 
+                               department: department2, user: user2)
+    product_category2 = ProductCategory.create!(name: 'Esporte e lazer')
+    product_subcategory2 = ProductSubcategory.create!(name: 'Bola', product_category: product_category2)
+    product_condition2 = ProductCondition.create!(name: 'Usado')
+    product2 = Product.new(name: 'Adidas Fevernova - Bola da copa de 2002', product_subcategory: product_subcategory2, 
+                           description: 'Bola de futebol oficial da Copa do Mundo FIFA de 2002 feita pela Adidas.', price: '240', 
+                           product_condition: product_condition2, quantity: '3', profile: profile2)
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova1.jpg')), filename: 'fevernova1.jpg')
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova2.jpg')), filename: 'fevernova2.jpg')
+    product2.save!
+    total_quantity = 2
+    purchased_product = PurchasedProduct.create!(product: product2, profile: profile1, total_quantity: '2', 
+                                                 initial_value: total_quantity * product2.price, start_date: '10/04/2030')
+    purchased_product.andamento!
+
+    login_as(user2, scope: :user)
+    visit root_path
+    click_on 'Vendas'
+
+    expect(page).to have_content('Vendas em negociação')
+    expect(page).to have_content('Adidas Fevernova - Bola da copa de 2002')
+    expect(page).to have_content('Eduardo Rodrigues')
+    expect(purchased_product.status).to eq 'andamento'
+  end
+
+  scenario 'and buyer visualize list of negotiation purchases' do
+    Company.create!(name: 'Rotes', cnpj: '96.672.638/0001-48', domain: 'rotes.com')
+    user1 = User.create!(email: 'eduardo@rotes.com', password: '120456')
+    user2 = User.create!(email: 'carol@rotes.com', password: '123456')
+    department1 = Department.create!(name: 'RH')
+    department2 = Department.create!(name: 'Vendas')
+    profile1 = Profile.create!(full_name: 'Eduardo Rodrigues', birthday: '10/03/1992', 
+                               work_address: 'Avenida Paulista, 170', position: 'Assistente', sector: 'Treinamento e desenvolvimento', 
+                               department: department1, user: user1)
+    profile2 = Profile.create!(full_name: 'Edgar Gomes', chosen_name: 'Carol Gomes', birthday: '10/08/1990', 
+                               work_address: 'Avenida Paulista, 170', position: 'Auxiliar', sector: 'Administração', 
+                               department: department2, user: user2)
+    product_category2 = ProductCategory.create!(name: 'Esporte e lazer')
+    product_subcategory2 = ProductSubcategory.create!(name: 'Bola', product_category: product_category2)
+    product_condition2 = ProductCondition.create!(name: 'Usado')
+    product2 = Product.new(name: 'Adidas Fevernova - Bola da copa de 2002', product_subcategory: product_subcategory2, 
+                           description: 'Bola de futebol oficial da Copa do Mundo FIFA de 2002 feita pela Adidas.', price: '240', 
+                           product_condition: product_condition2, quantity: '3', profile: profile2)
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova1.jpg')), filename: 'fevernova1.jpg')
+    product2.images.attach(io: File.open(Rails.root.join('app', 'assets', 'images', 'fevernova2.jpg')), filename: 'fevernova2.jpg')
+    product2.save!
+    total_quantity = 2
+    purchased_product = PurchasedProduct.create!(product: product2, profile: profile1, total_quantity: '2', 
+                                                 initial_value: total_quantity * product2.price, start_date: '10/04/2030')
+    purchased_product.andamento!
+
+    login_as(user1, scope: :user)
+    visit root_path
+    click_on 'Compras'
+
+    expect(page).to have_content('Compras em negociação')
+    expect(page).to have_content('Adidas Fevernova - Bola da copa de 2002')
+    expect(page).to have_content('Carol Gomes')
+    expect(purchased_product.status).to eq 'andamento'
+  end
 end
